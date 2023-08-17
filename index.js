ba6eb83b64b9c18cd220df687366129efb23eaff
Gustavo Verdugo <gverdugo@sb.cl>
2023-08-16 20:49:48 -0400

    update

---
+++ index.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql');
+const fs = require('fs');
+
+// Conexión a la base de datos
+const connection = mysql.createConnection({
+  host: '201.148.104.108',
+  user: 'corelabs_neo',
+  password: 'Maximiliano1998.',
+  database: 'corelabs_labsearch'
+});
+
+
+connection.connect();
+
+// Consulta SQL para obtener los datos
+const sqlQuery = 'SELECT product_name, brand_name, category_name, product_price FROM product';
+
+connection.query(sqlQuery, (error, results) => {
+  if (error) throw error;
+
+  // Calcular el número de categorías únicas y marcas únicas
+  const uniqueCategories = new Set(results.map(item => item.category_name));
+  const uniqueBrands = new Set(results.map(item => item.brand_name));
+
+  const num_categories = uniqueCategories.size;
+  const num_brands = uniqueBrands.size;
+
+  // Número de características numéricas (por ejemplo, "product_price")
+  const num_numeric_features = 1;  // Actualiza este valor según tus características numéricas
+
+  const preprocessedData = results.map(item => ({
+    product_name: item.product_name,
+    brand: item.brand_name,
+    category: item.category_name,
+    price: item.product_price
+  }));
+  console.log(results);
+  console.log('num_categories:', num_categories);
+  console.log('num_brands:', num_brands);
+  console.log('num_numeric_features:', num_numeric_features);
+
+  fs.writeFileSync('processed_data.json', JSON.stringify(preprocessedData, null, 2));
+  console.log('Procesamiento de datos completado y guardado en processed_data.json');
+});
+
+connection.end();
---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from rapidfuzz import fuzz
+import json
+
+app = Flask(__name__)
+
+# Cargar los datos desde el archivo JSON
+with open('processed_data.json', 'r') as file:
+    data = json.load(file)
+
+# Función para buscar productos en los datos
+def search_products(query, max_results=20):
+    results = []
+    for product in data:
+        product_name = product['product_name']
+        similarity_score = fuzz.partial_ratio(query.lower(), product_name.lower())
+        if similarity_score > 60:
+            results.append((product_name, similarity_score, product['price']))
+        
+        if len(results) >= max_results:
+            break
+    results.sort(key=lambda x: x[1], reverse=True)
+    return results
+
+@app.route('/search', methods=['GET'])
+def search():
+    search_query = request.args.get('text')
+    search_results = search_products(search_query, max_results=20)
+    
+    if search_results:
+        results = []
+        for result in search_results:
+            results.append({
+                "product": result[0],
+                "similarity": result[1],
+                "price": result[2]
+            })
+        return jsonify(results)
+    else:
+        return jsonify({"message": "No se encontraron resultados"})
+
+if __name__ == '_main_':
+    app.run(host='127.0.0.1', port=5000)
